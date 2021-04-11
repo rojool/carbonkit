c1f3a068c01e519a458679142261d09a3a184dc5
FINSOFT\mikael.grandsire <mikael.grandsire@finsoft.lu>
2021-04-11 23:28:05 +0200

    Init commit: Kit + Carbon libs

---
+++ src/routes/$layout.svelte
@@ -0,0 +1,15 @@
+<script>
+	import 'carbon-components-svelte/css/all.css';
+	import '../app.css';
+
+	import Theme from '$lib/Theme.svelte';
+	import UIshell from '$lib/UIshell.svelte';
+	import { Content } from 'carbon-components-svelte';
+</script>
+
+<Theme theme="white">
+	<UIshell />
+	<Content>
+		<slot />
+	</Content>
+</Theme>
---
+++ src/routes/index.svelte
@@ -0,0 +1,20 @@
+<script>
+	import {
+		Grid,
+		Row,
+		Column,
+	} from 'carbon-components-svelte';
+</script>
+
+<svelte:head>
+	<title>Hello</title>
+</svelte:head>
+
+<Grid>
+	<Row>
+		<Column>
+			<h1>Hello CarbonKit</h1>
+		</Column>
+	</Row>
+</Grid>
+
