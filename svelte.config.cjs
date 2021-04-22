447866d77eb475a853e4f52a9589ff413cf45e3b
Mikael Grandsire <mikael.grandsire@finsoft.lu>
2021-04-22 08:32:48 +0200

    Fix for clipboard-copie (add ref to issue)

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -9,6 +9,7 @@ module.exports = {
 
 		vite: {
 			optimizeDeps: {
+				// Include to fix cliboard-copie issue: https://github.com/IBM/carbon-components-svelte/issues/595#issuecomment-824562588
 				include: ['clipboard-copy']
 			},
 			ssr: {
